90a2593650e431092f8815edae6e4bf996058b70
hyeokgyu <gurrb1017@gmail.com>
2023-08-30 10:43:14 +0900

    not good example of constructor

--- Constructor1.cpp
+++ Constructor1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-// Constructor(생성자) 오버로딩, 디폴트 값 설정 예시
+// Class [변수명]();과 같은 방식은 생성자를 디폴트로 호출하는 것이 아닌 함수의 원형 선언으로 인식한다.
+// 그에대한 예시 코드
 using namespace std;
 
 class SimpleClass
@@ -8,35 +9,24 @@ private:
     int num1;
     int num2;
 public:
-    SimpleClass(){
-        num1 = 0;
-        num2 = 0;
-    }
-    SimpleClass(int n){
-        num1 = n;
-        num2 = 0;
-    }
-    SimpleClass(int n1, int n2){
+    SimpleClass(int n1 = 0, int n2 = 0){
         num1 = n1;
         num2 = n2;
     }
-    /*SimpleClass(int n1 = 0, int n2 = 0){
-        num1 = n1;
-        num2 = n2;
-    }*/
     void ShowData() const{
         cout<<num1<<' '<<num2<<endl;
     }
 };
 
 int main(void){
-    SimpleClass sc1;// 함수의 원형 선언과의 구별을 위해 SimpleClass sc1();과 같은 형태의 사용을 금지한다.
-    sc1.ShowData(); // 0 0
+    SimpleClass sc1();// 함수의 원형 선언으로 인식함
+    SimpleClass mysc = sc1();
 
-    SimpleClass sc2(100);
-    sc2.ShowData(); // 100 0
-
-    SimpleClass sc3(100, 200);
-    sc3.ShowData(); // 100 200
+    mysc.ShowData();
     return 0;
+}
+
+SimpleClass sc1(){ //함수 정의 부분
+    SimpleClass sc(20,30);
+    return sc;
 }
