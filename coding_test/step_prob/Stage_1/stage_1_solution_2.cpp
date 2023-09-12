152625d96d72960e1af561377d467f7b4869475b
hyeokgyu <gurrb1017@gmail.com>
2023-09-12 23:55:08 +0900

    #2588, #11382

--- coding_test/step_prob/Stage_1/stage_1_solution_2.cpp
+++ coding_test/step_prob/Stage_1/stage_1_solution_2.cpp
@@ -36,24 +36,93 @@
 // 출력 : 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C,
 //        넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
 // ==========================================================================
+// #include <iostream>
+// using namespace std;
+
+// void print_remain(int num1, int num2, int num3);
+
+// int main(void)
+// {
+//     int A(0), B(0), C(0);
+//     cin>>A>>B>>C;
+//     print_remain(A, B, C);
+
+//     return 0;
+// }
+
+// void print_remain(int num1, int num2, int num3)
+// {
+//     cout<<(num1+num2)%num3<<endl;
+//     cout<<((num1%num3)+(num2%num3))%num3<<endl;
+//     cout<<(num1*num2)%num3<<endl;
+//     cout<<((num1%num3)*(num2%num3))%num3<<endl;
+// }
+
+// ==========================================================================
+// #2588
+// 링크 : https://www.acmicpc.net/problem/2588
+// 문제 : (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+//        (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을
+//        구하는 프로그램을 작성하시오.
+// 입력 : 첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가,
+//        둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다.
+// 출력 : 첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
+// ==========================================================================
+// #include <iostream>
+// using namespace std;
+
+// void multiply(int num1, int num2);
+
+// int main(void)
+// {
+//     int num1(0), num2(0);
+//     cin>>num1;
+//     cin>>num2;
+//     multiply(num1, num2);
+
+//     return 0;
+// }
+
+// void multiply(int num1, int num2)
+// {
+//     int q1 = num2 % 10;
+//     int q2 = (num2/10)%10;
+//     int q3 = ((num2/10)/10);
+
+//     int multi1=num1*q1;
+//     int multi2=num1*q2;
+//     int multi3=num1*q3;
+
+//     int result=multi3*100+multi2*10+multi1;
+
+//     cout<<num1*q1<<endl;
+//     cout<<num1*q2<<endl;
+//     cout<<num1*q3<<endl;
+//     cout<<result<<endl;
+// }
+
+// ==========================================================================
+// #11382
+// 링크 : https://www.acmicpc.net/problem/11382
+// 문제 : 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+// 입력 : 첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 10^12)이 공백을 사이에 두고 주어진다.
+// 출력 : A+B+C의 값을 출력한다.
+// ==========================================================================
 #include <iostream>
 using namespace std;
 
-void print_remain(int num1, int num2, int num3);
+void plusfunc(long long num1, long long num2, long long num3);
 
 int main(void)
 {
-    int A(0), B(0), C(0);
-    cin>>A>>B>>C;
-    print_remain(A, B, C);
+    long long num1(0), num2(0), num3(0);
+    cin>>num1>>num2>>num3;
+    plusfunc(num1, num2, num3);
 
     return 0;
 }
 
-void print_remain(int num1, int num2, int num3)
+void plusfunc(long long num1, long long num2, long long num3)
 {
-    cout<<(num1+num2)%num3<<endl;
-    cout<<((num1%num3)+(num2%num3))%num3<<endl;
-    cout<<(num1*num2)%num3<<endl;
-    cout<<((num1%num3)*(num2%num3))%num3<<endl;
+    cout<<num1+num2+num3<<endl;
 }
