5cb3bfe0a1293cec8393935bc71d9a12cd0b73cb
hyeokgyu <gurrb1017@gmail.com>
2023-10-09 23:58:59 +0900

    #10950

--- coding_test/step_prob/Stage_3/stage_3_solution.cpp
+++ coding_test/step_prob/Stage_3/stage_3_solution.cpp
@@ -6,14 +6,49 @@
 // 입력 : 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
 // 출력 : 출력형식과 같게 N*1부터 N*9까지 출력한다.
 // ==========================================================================
+// #include <iostream>
+// using namespace std;
+
+// void Multiply(int n)
+// {
+//     for(int i=0; i < 9; i++)
+//     {
+//         cout<<n<<" * "<<i+1<<" = "<<n*(i+1)<<endl;
+//     }
+// }
+
+// int main(void)
+// {
+//     int n(0);
+//     cin>>n;
+//     Multiply(n);
+
+//     return 0;
+// }
+
+// ==========================================================================
+// #10950
+// 링크 : https://www.acmicpc.net/problem/10950
+// 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 입력 : 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//        각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 출력 : 각 테스트 케이스마다 A+B를 출력한다.
+// ==========================================================================
 #include <iostream>
+
 using namespace std;
 
-void Multiply(int n)
+void Add(int n)
 {
-    for(int i=0; i < 9; i++)
+    int arr_A[5];
+    int arr_B[5];
+    for(int i=0; i<n; i++)
     {
-        cout<<n<<" * "<<i+1<<" = "<<n*(i+1)<<endl;
+        cin>>arr_A[i]>>arr_B[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr_A[i]+arr_B[i]<<endl;
     }
 }
 
@@ -21,7 +56,7 @@ int main(void)
 {
     int n(0);
     cin>>n;
-    Multiply(n);
+    Add(n);
 
     return 0;
 }
