45a8cfd30f0f854635dc88cd92dadc5bad9a8521
hyeokgyu <gurrb1017@gmail.com>
2023-10-23 00:05:38 +0900

    #2562

--- coding_test/step_prob/Stage_4/stage_4_solution.cpp
+++ coding_test/step_prob/Stage_4/stage_4_solution.cpp
@@ -75,38 +75,81 @@
 //        1,000,000보다 작거나 같은 정수이다.
 // 출력 : 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
 // ==========================================================================
+// #include <iostream>
+
+// using namespace std;
+
+// int main(void)
+// {
+//     int n(0);
+//     cin>>n;
+//     int* arr = new int[n];
+//     for(int i=0; i<n; i++)
+//     {
+//         cin>>arr[i];
+//     }
+//     int mini(0), maxi(0);
+//     for(int i=0; i<n; i++)
+//     {
+//         if(i==0)
+//         {
+//             mini=arr[i];
+//             maxi=arr[i];
+//         }
+//         if(arr[i]<=mini)
+//         {
+//             mini=arr[i];
+//         }
+//         if(arr[i]>=maxi)
+//         {
+//             maxi=arr[i];
+//         }
+//     }
+
+//     cout<<mini<<" "<<maxi;
+
+//     return 0;
+// }
+
+// ==========================================================================
+// #2562
+// 링크 : https://www.acmicpc.net/problem/2562
+// 문제 : 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를
+//        구하는 프로그램을 작성하시오. 예를 들어, 서로 다른 9개의 자연수
+//        3, 29, 38, 12, 57, 74, 40, 85, 61 이 주어지면, 이들 중 최댓값은 85이고,
+//        이 값은 8번째 수이다.
+// 입력 : 첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다.
+//        주어지는 자연수는 100 보다 작다.
+// 출력 : 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+// ==========================================================================
 #include <iostream>
 
 using namespace std;
 
 int main(void)
 {
-    int n(0);
-    cin>>n;
-    int* arr = new int[n];
-    for(int i=0; i<n; i++)
+    int arr[9];
+    int max_num(0), index(0);
+    for(int i=0; i<9; i++)
     {
         cin>>arr[i];
     }
-    int mini(0), maxi(0);
-    for(int i=0; i<n; i++)
+
+    for(int i=0; i<9; i++)
     {
         if(i==0)
         {
-            mini=arr[i];
-            maxi=arr[i];
+            max_num=arr[i];
+            index=i+1;
         }
-        if(arr[i]<=mini)
+        if(max_num<=arr[i])
         {
-            mini=arr[i];
-        }
-        if(arr[i]>=maxi)
-        {
-            maxi=arr[i];
+            max_num=arr[i];
+            index=i+1;
         }
     }
-
-    cout<<mini<<" "<<maxi;
+    cout<<max_num<<"\n";
+    cout<<index;
 
     return 0;
 }
