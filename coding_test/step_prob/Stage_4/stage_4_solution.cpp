b2b55e75248b691ec194ee8d792783262aa13bf2
hyeokgyu <gurrb1017@gmail.com>
2023-10-24 13:41:50 +0900

    #5597

--- coding_test/step_prob/Stage_4/stage_4_solution.cpp
+++ coding_test/step_prob/Stage_4/stage_4_solution.cpp
@@ -217,33 +217,69 @@
 //        (1 ≤ i ≤ j ≤ N) 도현이는 입력으로 주어진 순서대로 공을 교환한다.
 // 출력 : 1번 바구니부터 N번 바구니에 들어있는 공의 번호를 공백으로 구분해 출력한다.
 // ==========================================================================
+// #include <iostream>
+// using namespace std;
+
+// int main(void)
+// {
+//     int n(0), m(0);
+//     cin>>n>>m;
+//     int* basket = new int[n](); //() -> 0으로 초기화
+//     for(int i=0; i<n; i++)
+//     {
+//         basket[i]=i+1;
+//     }
+//     int swap_1(0), swap_2(0), temp(0);
+//     for(int i=0; i<m; i++)
+//     {
+//         cin>>swap_1>>swap_2;
+//         temp = basket[swap_1-1];
+//         basket[swap_1-1] = basket[swap_2-1];
+//         basket[swap_2-1] = temp;
+//     }
+
+//     for(int i=0; i<n; i++)
+//     {
+//         cout<<basket[i]<<" ";
+//     }
+
+//     delete[] basket;
+
+//     return 0;
+// }
+
+// ==========================================================================
+// #5597
+// 링크 : https://www.acmicpc.net/problem/5597
+// 문제 : X대학 M교수님은 프로그래밍 수업을 맡고 있다. 교실엔 학생이 30명이 있는데,
+//        학생 명부엔 각 학생별로 1번부터 30번까지 출석번호가 붙어 있다.
+//        교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를
+//        구하는 프로그램을 작성하시오.
+// 입력 : 입력은 총 28줄로 각 제출자(학생)의 출석번호 n(1 ≤ n ≤ 30)가 한 줄에 하나씩 주어진다.
+//        출석번호에 중복은 없다.
+// 출력 : 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고,
+//        2번째 줄에선 그 다음 출석번호를 출력한다.
+// ==========================================================================
 #include <iostream>
+
 using namespace std;
 
 int main(void)
 {
-    int n(0), m(0);
-    cin>>n>>m;
-    int* basket = new int[n](); //() -> 0으로 초기화
-    for(int i=0; i<n; i++)
+    int arr[30]={0, };
+    int n(0);
+    for(int i=0; i<28; i++)
     {
-        basket[i]=i+1;
-    }
-    int swap_1(0), swap_2(0), temp(0);
-    for(int i=0; i<m; i++)
-    {
-        cin>>swap_1>>swap_2;
-        temp = basket[swap_1-1];
-        basket[swap_1-1] = basket[swap_2-1];
-        basket[swap_2-1] = temp;
+        cin>>n;
+        arr[n-1]=1;
     }
 
-    for(int i=0; i<n; i++)
+    for(int i=0; i<30; i++)
     {
-        cout<<basket[i]<<" ";
+        if(arr[i]!=1)
+        {
+            cout<<i+1<<endl;
+        }
     }
-
-    delete[] basket;
-
     return 0;
 }
