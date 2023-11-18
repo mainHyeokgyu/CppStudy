baef94222070fdc1902a50d787e127ad1ad40787
hyeokgyu <gurrb1017@gmail.com>
2023-11-18 22:53:58 +0900

    #11654

--- coding_test/step_prob/Stage_5/stage_5_solution.cpp
+++ coding_test/step_prob/Stage_5/stage_5_solution.cpp
@@ -50,20 +50,41 @@
 //        공백은 없으며 문자열의 길이는 1000보다 작다.
 // 출력 : 각 테스트 케이스에 대해서 주어진 문자열의 첫 글자와 마지막 글자를 연속하여 출력한다.
 // ==========================================================================
+// #include <iostream>
+
+// using namespace std;
+
+// int main(void)
+// {
+//     int test_num(0);
+//     cin>>test_num;
+//     string *ss = new string[test_num];
+//     string temp;
+//     for(int i=0; i<test_num; i++)
+//     {
+//         cin>>temp;
+//         cout<<temp[0]<<temp[temp.length()-1]<<endl;
+//     }
+//     return 0;
+// }
+
+// ==========================================================================
+// #11654
+// 링크 : https://www.acmicpc.net/problem/11654
+// 문제 : 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때,
+//        주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+// 입력 : 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+// 출력 : 입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+// ==========================================================================
 #include <iostream>
 
 using namespace std;
 
 int main(void)
 {
-    int test_num(0);
-    cin>>test_num;
-    string *ss = new string[test_num];
-    string temp;
-    for(int i=0; i<test_num; i++)
-    {
-        cin>>temp;
-        cout<<temp[0]<<temp[temp.length()-1]<<endl;
-    }
+    char c;
+    cin>>c;
+    cout<<(int)c<<endl;
+
     return 0;
 }
